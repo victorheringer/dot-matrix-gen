e449a385d1d84e9d43a1b198bedbe6d4dd05080b
Victor Heringer <victorhugo.heringer@hotmail.com>
2022-06-20 21:13:25 -0300

    Config build and routes for gh pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dot-matrix-gen">
       <App />
     </BrowserRouter>
   </React.StrictMode>
