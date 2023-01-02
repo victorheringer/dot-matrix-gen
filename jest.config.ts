70808a7759d682457df7ba99b86334938febcc58
Victor Heringer <victorhugo.heringer@hotmail.com>
2023-01-02 02:26:46 -0300

    Remove some files from coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,13 @@ export default {
     "^.+\\.tsx?$": "ts-jest",
   },
   coverageReporters: ["json-summary", "text", "lcov"],
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/**/styled.ts",
+    "!src/**/main.tsx",
+    "!src/**/App.tsx",
+  ],
   collectCoverage: true,
   testEnvironment: "jsdom",
 };
